13e576e917c5d7bafc738f24b0c0f9ece5111ea8
Fernando Ares <ferares4@gmail.com>
2015-04-11 16:12:07 -0300

    Commit

--- ejemplo1/src/fiuba/algo3/ejemplo1/Foo.java
+++ ejemplo1/src/fiuba/algo3/ejemplo1/Foo.java
@@ -2,10 +2,17 @@ package ejemplo1.src.fiuba.algo3.ejemplo1;
 
 public class Foo {
 
+	String mensaje;
+	
+	public Foo(){
+		
+		mensaje = "FOO"
+		
+	}
 	
 	String doFoo(){
 		
-		return "FOO";
+		return mensaje;
 		
 	}
 
--- ejemplo1/test/fiuba/algo3/ejemplo1/FooTest.java
+++ ejemplo1/test/fiuba/algo3/ejemplo1/FooTest.java
@@ -7,9 +7,9 @@ public class FooTest {
 
 	@Test
 	public void doFooShouldReturnFoo(){
-        //Foo foo = new Foo();
-        //String result = foo.doFoo();
-        //Assert.assertEquals("Foo", result);
+        Foo foo = new Foo();
+        String result = foo.doFoo();
+        Assert.assertEquals("Foo", result);
 
 	}
 
