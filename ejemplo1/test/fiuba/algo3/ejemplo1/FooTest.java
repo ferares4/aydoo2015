27d501ab4034501941419155b73687726c1c97e7
Fernando Ares <ferares4@gmail.com>
2015-04-11 16:03:43 -0300

    Commit

--- ejemplo1/test/fiuba/algo3/ejemplo1/FooTest.java
+++ ejemplo1/test/fiuba/algo3/ejemplo1/FooTest.java
@@ -7,8 +7,8 @@ public class FooTest {
 
 	@Test
 	public void doFooShouldReturnFoo(){
-        Foo foo = new Foo();
-        String result = foo.doFoo();
+        //Foo foo = new Foo();
+        //String result = foo.doFoo();
         Assert.assertEquals("Foo", result);
 
 	}
