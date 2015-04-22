4e430a41185f8e56b1f8f4069e93e2c9046a7741
Fernando Ares <ferares4@gmail.com>
2015-04-22 15:10:25 -0300

    TP1

---
+++ trabajoPracticoN1/src/Atraccion.java
@@ -0,0 +1,50 @@
+
+public class Atraccion {
+	
+	private int coordenadaX;
+	private int coordenadaY;
+	private double costo;
+	private int cupoDeIntegrantesDiarios;
+	private int tiempoNecesarioParaVisita;
+	private String tipo;
+	
+	public int getCoordenadaX() {
+		return coordenadaX;
+	}
+	public void setCoordenadaX(int coordenadaX) {
+		this.coordenadaX = coordenadaX;
+	}
+	public int getCoordenadaY() {
+		return coordenadaY;
+	}
+	public void setCoordenadaY(int coordenadaY) {
+		this.coordenadaY = coordenadaY;
+	}
+	public double getCosto() {
+		return costo;
+	}
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	public int getCupoDeIntegrantesDiarios() {
+		return cupoDeIntegrantesDiarios;
+	}
+	public void setCupoDeIntegrantesDiarios(int cupoDeIntegrantesDiarios) {
+		this.cupoDeIntegrantesDiarios = cupoDeIntegrantesDiarios;
+	}
+	public int getTiempoNecesarioParaVisita() {
+		return tiempoNecesarioParaVisita;
+	}
+	public void setTiempoNecesarioParaVisita(int tiempoNecesarioParaVisita) {
+		this.tiempoNecesarioParaVisita = tiempoNecesarioParaVisita;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
--- trabajoPracticoN1/test/AtraccionTest.java
+++ trabajoPracticoN1/test/AtraccionTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 
 public class AtraccionTest {
@@ -7,11 +8,19 @@ public class AtraccionTest {
 	public void getAtraccionTest(){
 		
 		Atraccion miAtraccion = new Atraccion();
-		Assert.assertEquals("192", miAtraccion.getCoordenadas());
-		Assert.assertEquals("10", miAtraccion.getCosto());
-		Assert.assertEquals("8", miAtraccion.getTiempoNecesarioParaVisita());
-		Assert.assertEquals("400", miAtraccion.getCupoDeIntegrantesDiarios());
-		Assert.assertEquals("Paisaje de Aventura", miAtraccion.Tipo());
+		miAtraccion.setCoordenadaX(192);
+		miAtraccion.setCoordenadaY(192);
+		miAtraccion.setCosto(10);
+		miAtraccion.setTiempoNecesarioParaVisita(8);
+		miAtraccion.setCupoDeIntegrantesDiarios(400);
+		miAtraccion.setTipo("Paisaje de Aventura");
+		
+		Assert.assertEquals(192, miAtraccion.getCoordenadaX());
+		Assert.assertEquals(192, miAtraccion.getCoordenadaY());
+		Assert.assertEquals(10,0, miAtraccion.getCosto());
+		Assert.assertEquals(8, miAtraccion.getTiempoNecesarioParaVisita());
+		Assert.assertEquals(400, miAtraccion.getCupoDeIntegrantesDiarios());
+		Assert.assertEquals("Paisaje de Aventura", miAtraccion.getTipo());
 		
 		
 	}
